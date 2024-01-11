f268951e1469643daffc919ed907e018bfb27127
Kiprotich Davis <mylesdavyz@gmail.com>
2024-01-11 08:18:29 +0300

    Created a template

--- app/template.jsx
+++ app/template.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import useScrollProgress from "@/hooks/useScrollProgress";
 import { motion } from "framer-motion";
+import useScrollProgress from "@/hooks/useScrollProgress";
 
 const variants = {
   hidden: { opacity: 0 },
@@ -14,12 +14,19 @@ const Template = ({ children }) => {
     <>
       <motion.main
         variants={variants}
-        initial={hidden}
-        animate={enter}
+        initial="hidden"
+        animate="enter"
         transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
       >
         {children}
       </motion.main>
+      {/* completion bar */}
+      <span
+        style={{ transform : `translateY(${completion - 100}%)` }}
+        className="fixed z-50 bg-primary w-1 top-0 right-0 
+      bottom-0 transition-all duration-700"
+      ></span>
+      <div className="h-[4000px]"></div>
     </>
   );
 };
