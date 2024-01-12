fc5b29ec92116bf60b745f617a4107668d330b4f
Kiprotich Davis <mylesdavyz@gmail.com>
2024-01-12 09:19:13 +0300

    Mobile Navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,27 @@
+'use client'
+import React, { useState, useEffect } from "react"
+
+import Logo from "./Logo"
 import ThemeToggler from "./ThemeToggler"
+import Nav from "./Nav"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
     <header>
-        <ThemeToggler />
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div>
+            <Nav />
+            <ThemeToggler />
+            {/* mobile navigation */}
+            <div>
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
